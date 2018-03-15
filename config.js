5292892a9cfc3514a472a89de2d2b8fb27c63048
Ethan <ethandeguire@gmail.com>
2018-03-14 23:55:04 -0400

    working end user update
    -effieciency in frame drawing improvements
    -outliers and outlier toggle button ACTUALLY working
    -button that calls a prompt to have the user input data, which is then parsed and graphed

--- config.js
+++
@@ -1,8 +0,0 @@
-//github.com/ethandeguire/Stats
-//change these numbers to edit dataset, separate by commas
-//SHOULD ONLY AFFECT DEFAULT VALUES, NOT PERMANENT
-
-var OGdataSet = [1,1,1,1,4,4,5,7,7,8,9,9,9,1,2,2,11,12,13,14,-1,-2,-3,-3,-3,17,18,3,24,25,3,2,4,5,5,3,6,5,4,0,2,2,2,19,200,200];
-var roundFactor = 10; //value to round  by - higher number == more precise. use powers of 10
-var customHistoWidth = 20; //value for horizontal ranges in histogram, only valid if graphMode = "custom"
-var maxNums = 20; //max numbers allowed on histograph
--- sketch.js
+++ sketch.js
@@ -1,19 +1,22 @@
 //github.com/ethandeguire/Stats
-var graphMode =  "Standard Deviation Mode"; // "Custom Mode" "All Points Mode" "Standard Deviation Mode"
-var includeOutliers = false; //false or true
+//change these numbers to edit dataset, separate by commas
 
-var dataSetSize; //used to be arrSize
+var UserDataSet = [2, 2, 2, 5, 6, 6, 6, 6, 7, 8, 9, 10, 11, 11, 11, 11];
+var dataSet;
+// "0" = all points historammed ----- "1" = standard deviation histogram ----- "2" = custom size histogram
+var graphMode = 1;
+var roundFactor = 10; //value to round  by - higher number == more precise. use powers of 10
+var customHistoWidth = 2; //value for horizontal ranges in histogram, only valid if graphMode = "custom"
+var maxNums = 20; //max numbers allowed on histograph
+var includeOutliers = true; //false or true
 
-var dataSetNums = []; //used to be arrNums
-var dataSetFreqs = []; //used to be arrFreqs
+var arrSize;
 
-var stdDevGroups = [];
-var stdDevCount = [];
+var devGroups = [];
+var devCount = [];
 
-var customSizeGroups = [];
-var customSizeCount = [];
-
-
+var custGroups = [];
+var custCount = [];
 
 var sumSqr = 0;
 var dataSum = 0;
@@ -22,194 +25,139 @@ var stdDev;
 var Median;
 var Q1;
 var Q3;
-
+var arrNums;
+var arrFreqs;
 var relFreqs;
 var maxNum;
 var minimumVal;
 var maximumVal;
 var result;
-var outLiers = 0;
+var outliers = 0;
 
-var selGraphMode;
+function freq() {
+	var a = [],
+		b = [],
+		prev;
 
-function createSliders()
-{
-	
-	selGraphMode = createSelect();
-	selGraphMode.position(20,410);
-	selGraphMode.option("Standard Deviation Mode");
-	selGraphMode.option("All Points Mode");
-	//selGraphMode.option("Custom Mode");
-	selGraphMode.changed(selGraphModeFunc);
-	
-	
-	toggleOutliers = createButton("Toggle Outliers");
-	toggleOutliers.position(20,450);
-	toggleOutliers.mousePressed(outlierToggleFunc);
+	for (var i = 0; i < dataSet.length; i++) {
+		if (dataSet[i] !== prev) {
+			a.push(dataSet[i]);
+			b.push(1);
+		} else {
+			b[b.length - 1]++;
+		}
+		prev = dataSet[i];
+	}
 
+	return [a, b];
 }
 
-function selGraphModeFunc(){graphMode = selGraphMode.value();}
-function outlierToggleFunc(){if (includeOutliers == true){includeOutliers = false}else if(includeOutliers == false){includeOutliers = true}}
-
-
-
-function deleteStuff()
-{
-	console.log(CustomHistoInput.value());
-	customHistoWidth = int(CustomHistoInput.value());	
-	CustomHistoInput.remove();
-	submitButton.remove();
-}
-
-
-
-
-function freq() 
-{
-    var a = [], b = [], prev;
-
-    for ( var i = 0; i < dataSet.length; i++ ) {
-        if ( dataSet[i] !== prev ) {
-            a.push(dataSet[i]);
-            b.push(1);
-        } else {
-            b[b.length-1]++;
-        }
-        prev = dataSet[i];
-    }
-
-    return [a, b];
-}
-
-function sortArr()
-{
+function sortArr() {
 	var count = 0;
-	for (var b = 0; b<(dataSetSize-1); ++b){
-		for (var a = 0; a<(dataSetSize-1); ++a){
+	for (var b = 0; b < arrSize - 1; ++b) {
+		for (var a = 0; a < arrSize - 1; ++a) {
 			count += 1;
-			if (dataSet[a] > dataSet[(a+1)])
-			{
+			if (dataSet[a] > dataSet[a + 1]) {
 				var temp = dataSet[a];
-				dataSet[a] = dataSet[(a+1)];
-				dataSet[a+1] = temp;
+				dataSet[a] = dataSet[a + 1];
+				dataSet[a + 1] = temp;
 			}
 		}
-	}	
+	}
 }
 
-function quartile(startPos, endPos)
-{
+function quartile(startPos, endPos) {
 	var range = endPos - startPos + 1;
-	if (range % 2 == 1){
-		var elimsQ = int(range/2);
-		return dataSet[elimsQ+startPos];
-	}else{
-		var elimsQ = range/2;
+	if (range % 2 == 1) {
+		var elimsQ = int(range / 2);
+		return dataSet[elimsQ + startPos];
+	} else {
+		var elimsQ = range / 2;
 		elimsQ += -1;
-		return ((dataSet[elimsQ+startPos]+dataSet[elimsQ+startPos+1]) /2)
+		return (dataSet[elimsQ + startPos] + dataSet[elimsQ + startPos + 1]) / 2;
 	}
-
 }
 
-function median()
-{
-	if (dataSetSize % 2 == 1){
-		var elims = int(dataSetSize/2);
+function median() {
+	if (arrSize % 2 == 1) {
+		var elims = int(arrSize / 2);
 		Median = dataSet[elims];
-		Q1 = quartile(0,elims-1);
-		Q3 = quartile(elims+1,dataSetSize-1);
-	}else{
-		var elims = dataSetSize/2;
+		Q1 = quartile(0, elims - 1);
+		Q3 = quartile(elims + 1, arrSize - 1);
+	} else {
+		var elims = arrSize / 2;
 		elims += -1;
-		Median = (dataSet[elims]+dataSet[elims+1])/2;
-		Q1 = quartile(0,elims);
-		Q3 = quartile(elims+1,dataSetSize-1);
+		Median = (dataSet[elims] + dataSet[elims + 1]) / 2;
+		Q1 = quartile(0, elims);
+		Q3 = quartile(elims + 1, arrSize - 1);
 	}
 }
 
-function setup() 
-{
-	createCanvas(800,400);
-	createSliders();
+function setup() {
+	everythingOnce();
+	
+	button = createButton("New Data Set");
+  button.position(140,387);
+  button.mouseClicked(newData);
+  
+  toggleOutliers = createButton("Toggle Outliers");
+	toggleOutliers.position(20,387);
+	toggleOutliers.mousePressed(outlierToggleFunc);
 }
 
-function doMath()
-{
-	//these need to be recleared if doMath() is called more than once per instance
-	dataSum = 0;
-	sumSqr = 0;
-	stdDevCount = [];
-	stdDevGroups = [];
-	
-	//working standard deviation code - compacted
-	for(var i=0;i<dataSetSize;i++){dataSum=dataSum+dataSet[i];}
-	avg=dataSum/dataSetSize;
-	for(var i=0; i<dataSetSize;i++){sumSqr=sumSqr+((dataSet[i]-avg)*(dataSet[i]-avg));}
-	stdDev=sqrt(sumSqr/dataSetSize);
-	
-	median();
-	minimumVal = dataSet[0];
-	maximumVal = dataSet[dataSetSize-1];
-	
-	//finds relative frequency for each unique number in data set
-	var temp = freq();
-	dataSetNums = temp[0];
-	dataSetFreqs = temp[1];
-	uniqueNums = dataSetNums.length;
-	relFreqs = [];
-	for(var i=0;i<dataSetNums.length;i++){relFreqs[i] = dataSetFreqs[i] / dataSetSize;}
-	maxNum = dataSetNums[dataSetNums.length-1];
-	
-
-	//for std dev horizontal
-	dataRange = dataSet[dataSetSize-1] - dataSet[0];
-	for(var i = 0; i<(dataRange/stdDev); i++){
-		stdDevGroups[i] = round ( roundFactor * (stdDev * i + stdDev + minimumVal))/roundFactor;
-		stdDevCount[i] = 0;
-	}
-	
-	for(var i = 0; i<dataSetSize; i++){
-		for (var j = 0; j<stdDevGroups.length; j++){
-			if (dataSet[i] < stdDevGroups[j] && dataSet[i] >= stdDevGroups[j] - round(roundFactor*stdDev)/roundFactor){
-				stdDevCount[j] += 1;
-			}
-		}
-	}
-	
-	//for custom horizontal
-	for(var i = 0; i<(dataRange/customHistoWidth); i++){
-		customSizeGroups[i] = round(roundFactor * (customHistoWidth * i + customHistoWidth + minimumVal))/roundFactor;     //round ( roundFactor * (stdDev * i + stdDev + minimumVal))/roundFactor;
-		customSizeCount[i] = 0;
-	}
-	
-	for(var i = 0; i<dataSetSize; i++){
-		for (var j = 0; j<customSizeGroups.length; j++){
-			if (dataSet[i] < customSizeGroups[j] && dataSet[i] >= customSizeGroups[j] - round(roundFactor*customHistoWidth)/roundFactor){
-				customSizeCount[j] += 1;
-			}
-		}
-	}
+function outlierToggleFunc(){
+  if (includeOutliers == true){
+    includeOutliers = false
+  }
+  else if(includeOutliers == false){
+    includeOutliers = true
+  }
+  
+  everythingOnce();
 }
 
-function draw() 
-{
+function newData(){
+  var txt = prompt("New data set, separate with commas");
+  tmpVar = split(txt, ",");
+  for (var i = 0; i<tmpVar.length; i++)
+  {
+    tmpVar[i] = tmpVar[i] - 1;
+    tmpVar[i] = tmpVar[i] + 1;
+  }
+  
+  UserDataSet = tmpVar;
+  everythingOnce();
+}
+
+function everythingOnce() {
+  dataSet = UserDataSet;
+  
+  sumSqr = 0;
+  dataSum = 0;
+  outLiers = 0;
+  devGroups = [];
+  devCount = [];
+  custGroups = [];
+  custCount = [];
+
+	createCanvas(800, 415);
 	background(120);
-	
-	
-	outLiers = 0;
-	
-	dataSet = OGdataSet;
-	dataSetSize = dataSet.length;
+	stroke(1);
+	strokeWeight(1);
+	arrSize = dataSet.length;
 	sortArr();
-	
-	doMath();
+  
+  
+  
+  doMath();
+  
+  
 	if (includeOutliers == false)
 	{
 		var tempArray = [];
 		var outRangeTop = Q3+(1.5*(Q3-Q1));
 		var outRangeBottom = Q1-(1.5*(Q3-Q1));
-		for(var i = 0; i<(dataSetSize); i++){
+		for(var i = 0; i<(arrSize); i++){
 			if (dataSet[i] > outRangeTop || dataSet[i] < outRangeBottom){
 				outLiers += 1;
 			}else{
@@ -218,143 +166,230 @@ function draw()
 		}
 		
 		dataSet = tempArray;
-		dataSetSize = dataSet.length;	
+		arrSize = dataSet.length;	
+		
 		doMath();
 	}
 
-	/*
-	console.log("Sum of Values: ",dataSum);
-	console.log("Avg: ",avg);
-	console.log("Standard Deviation: ",stdDev);
-	console.log("Quartile 1: ",Q1);
-	console.log("Median: ",Median);
-	console.log("Quartile 3: ",Q3);
-	console.log("Values ",dataSetSize);
-	console.log("Data Set:", dataSet);
-	console.log("Data Set Freqs:", dataSetFreqs);
-	console.log("Data Set Nums:", dataSetNums);
-	console.log("stdDevGroups:", stdDevGroups);
-	console.log("stdDevCount:", stdDevCount);
-	console.log("customSizeGroups:", customSizeGroups);
-	console.log("customSizeCount:", customSizeCount);
-	console.log("outliers: ",outLiers);	
-	*/
-	
-	var textNum = [10]; // whitespace is very important below
-	textNum[0] = "Sum of Values: 	     " + dataSum;
-	textNum[1] = "Average:                   " + round(10000*avg)/10000;
-	textNum[2] = "Std Deviation:   	     " + round(10000*stdDev)/10000;
-	textNum[3] = "1st Quartile:   	        " + Q1;
-	textNum[4] = "Median:                     "+ Median;
-	textNum[5] = "3rd Quartile:  	         " + Q3;
-	textNum[6] = "# of Values    	         " + dataSetSize;
-	textNum[7] = "Minimum    	            " + minimumVal;
-	textNum[8] = "Maximum    	           " + maximumVal;
-	textNum[9] = "Outliers Excluded    " + outLiers;
-	
+
+
+	console.log('Sum of Values: ', dataSum);
+	console.log('Avg: ', avg);
+	console.log('Standard Deviation: ', stdDev);
+	console.log('Quartile 1: ', Q1);
+	console.log('Median: ', Median);
+	console.log('Quartile 3: ', Q3);
+	console.log('Values ', arrSize);
+	console.log('Data Set:', dataSet);
+	//console.log("Frequencies", arrFreqs);
+	//console.log("Deviation Groups", devGroups);
+	//console.log("Deviation Group Counts", devCount);
+	//console.log("Custom Groups", custGroups);
+	//console.log("Custom Group Counts", custCount);
+
+	var text1 = 'Sum of Values:	     ' + dataSum;
+	var text2 = 'Average:                 ' + avg;
+	var text3 = 'Std Deviation:  	     ' + round(10000 * stdDev) / 10000;
+	var text4 = '1st Quartile:  	       ' + Q1;
+	var text5 = 'Median:         	        ' + Median;
+	var text6 = '3rd Quartile:  	       ' + Q3;
+	var text7 = '# of Values    	       ' + arrSize;
+	var text8 = 'Minimum    	           ' + minimumVal;
+	var text9 = 'Maximum    	         ' + maximumVal;
+
 	fill(255);
 	stroke(120);
-	
-	
-	//decrease this if more vars are added to textNum
-	var textY = 27;
 	textSize(15);
-	for (var i = 0; i<textNum.length; i++){text(textNum[i], 530, 40 + textY * i);}	
-
-	text(round(1000*minimumVal)/1000,16,365);
-	text(round(1000*maximumVal)/1000,514,365);	
+	text(text1, 530, 40);
+	text(text2, 530, 70);
+	text(text3, 530, 100);
+	text(text4, 530, 130);
+	text(text5, 530, 160);
+	text(text6, 530, 190);
+	text(text7, 530, 220);
+	text(text8, 530, 250);
+	text(text9, 530, 280);
 	
 	var graphBoxW = 500;
 	var graphBoxH = 350;
-	
+
 	noStroke();
 	fill(255);
-	rect(20,20,graphBoxW+20,graphBoxH);
-	
+	rectMode(CORNER);
+	rect(20, 20, graphBoxW, graphBoxH + 11);
+
 	rectMode(CORNER);
 	textAlign(LEFT);
 
 	//changes max numbers to write on histograph
-	if ((graphMode == 0 && dataSetFreqs.length > maxNums) || (graphMode == 1 && stdDevGroups.length > maxNums) ||(graphMode == 2 && customSizeGroups.length > maxNums)){}else {var doNumbers = true};
-	
+	if (
+		(graphMode == 0 && dataSet.length > maxNums) ||
+		(graphMode == 1 && devGroups.length > maxNums) ||
+		(graphMode == 2 && custGroups.length > maxNums)
+	) {
+	} else {
+		var doNumbers = true;
+	}
+
 	//draw histogram boxes in different colors according to their size.
-	if (graphMode == "All Points Mode")
-	{
-		var colConst = 200/dataSetNums.length;
-		var histWidth = graphBoxW/dataSetNums.length;
-		var oneSize = (graphBoxH-20) / max(dataSetFreqs);
-		
-		for (var i = 0; i<dataSetNums.length; ++i)
-		{
-			stroke(i*colConst+55,0,0);
-			fill(i*colConst+55,0,0);
-			rect(20+histWidth*i,height-51,histWidth,-oneSize*dataSetFreqs[i]);
+	if (graphMode == 0) {
+		var colConst = 200 / arrNums.length;
+		var histWidth = graphBoxW / arrNums.length;
+		var oneSize = (graphBoxH - 20) / max(arrFreqs);
+
+		for (var i = 0; i < arrNums.length; ++i) {
+			stroke(i * colConst + 55, 0, 0);
+			fill(i * colConst + 55, 0, 0);
+			rect(20 + histWidth * i, 380, histWidth, -oneSize * arrFreqs[i]);
 			fill(255);
-			if (doNumbers == true){var temp2 = round(100*dataSetNums[i])/100+' ';text(temp2, 12+histWidth*i+histWidth/2,height-51)}
+			if (doNumbers == true) {
+				var temp2 = round(100 * arrNums[i]) / 100 + ' ';
+				text(temp2, 12 + histWidth * i + histWidth / 2, 380);
+			}
 		}
-	}else if (graphMode == "Standard Deviation Mode"){
-		var colConst = 200/stdDevGroups.length;
-		var histWidth = graphBoxW/stdDevGroups.length;
-		var oneSize = (graphBoxH-20) / max(stdDevCount);
-		
-		for (var i = 0; i<stdDevGroups.length; ++i)
-		{
-			stroke(i*colConst+55,0,0);
-			fill(i*colConst+55,0,0);
-			rect(20+histWidth*i,height-51,histWidth,-oneSize*stdDevCount[i]);
+	} else if (graphMode == 1) {
+		var colConst = 200 / devGroups.length;
+		var histWidth = graphBoxW / devGroups.length;
+		var oneSize = (graphBoxH - 20) / max(devCount);
+
+		for (var i = 0; i < devGroups.length; ++i) {
+			stroke(i * colConst + 55, 0, 0);
+			fill(i * colConst + 55, 0, 0);
+			rect(20 + histWidth * i, 380, histWidth, -oneSize * devCount[i]);
 			fill(255);
-			if (doNumbers == true){if (i != 0){var temp3 = round(100*(stdDevGroups[i-1]))/100 + '-' + round(100*(stdDevGroups[i]))/100;}else{var temp3 = round(100*minimumVal)/100 + '-' + round(100*stdDevGroups[i])/100;}text(temp3, -10+histWidth*i+histWidth/2,height-51)}
+			if (doNumbers == true) {
+				if (i != 0) {
+					var temp3 =
+						round(100 * devGroups[i - 1]) / 100 +
+						'-' +
+						round(100 * devGroups[i]) / 100;
+				} else {
+					var temp3 =
+						round(100 * minimumVal) / 100 +
+						'-' +
+						round(100 * devGroups[i]) / 100;
+				}
+				text(temp3, -10 + histWidth * i + histWidth / 2, 380);
+			}
 		}
-	
-	}/*else if (graphMode == "Custom Mode"){
-		var colConst = 200/customSizeGroups.length;
-		var histWidth = graphBoxW/customSizeGroups.length;
-		var oneSize = (graphBoxH-20) / max(customSizeCount);
-		
-		for (var i = 0; i<customSizeGroups.length; ++i)
-		{
-			stroke(i*colConst+55,0,0);
-			fill(i*colConst+55,0,0);
-			rect(20+histWidth*i,height-20,histWidth,-oneSize*customSizeCount[i]);
+	} else if (graphMode == 2) {
+		var colConst = 200 / custGroups.length;
+		var histWidth = graphBoxW / custGroups.length;
+		var oneSize = (graphBoxH - 20) / max(custCount);
+
+		for (var i = 0; i < custGroups.length; ++i) {
+			stroke(i * colConst + 55, 0, 0);
+			fill(i * colConst + 55, 0, 0);
+			rect(20 + histWidth * i, 380, histWidth, -oneSize * custCount[i]);
 			fill(255);
-			//if (doNumbers == true){if (i != 0){var temp3 = round(100*(customSizeGroups[i-1]))/100 + '-' + round(100*(customSizeGroups[i]))/100;}else{var temp3 = round(100*minimumVal)/100 + '-' + round(100*customSizeGroups[i])/100;}text(temp3, -10+histWidth*i+histWidth/2,height-20)}
+			if (doNumbers == true) {
+				if (i != 0) {
+					var temp3 =
+						round(100 * custGroups[i - 1]) / 100 +
+						'-' +
+						round(100 * custGroups[i]) / 100;
+				} else {
+					var temp3 =
+						round(100 * minimumVal) / 100 +
+						'-' +
+						round(100 * custGroups[i]) / 100;
+				}
+				text(temp3, -10 + histWidth * i + histWidth / 2, 380);
+			}
 		}
-	}*/
-	
-	
+	}
+
 	noStroke();
 	fill(255);
-	rect(535,300,245,70);
-	
+	rect(535, 300, 245, 70);
+
 	//have 245 pixels = 100%
 	// 100% should be max value, highest in dataset
-	
-	var newConst = 245/(dataSet[dataSetSize-1]+1);
-	var Q3rd = (Q3*newConst)+535;
-	var Q1st = (Q1*newConst)+535;
-	
+
+	var newConst = 245 / (dataSet[arrSize - 1] + 1);
+	var Q3rd = Q3 * newConst + 535;
+	var Q1st = Q1 * newConst + 535;
+
 	rectMode(CORNERS);
-	fill(255,0,10);
-	rect(Q3rd,315,Q1st,355);
-	rect(Q3rd,334,780,336);
-	rect(535+(dataSet[0]*i),334,Q1st,336);
+	fill(255, 0, 10);
+	rect(Q3rd, 315, Q1st, 355);
+	rect(Q3rd, 334, 780, 336);
+	rect(535 + dataSet[0] * i, 334, Q1st, 336);
 	fill(0);
-	rect(Median*newConst+534,315,Median*newConst+536,355)
+	rect(Median * newConst + 534, 315, Median * newConst + 536, 355);
 }
 
+function doMath(){
+  sumSqr = 0;
+  dataSum = 0;
+  outLiers = 0;
+  devGroups = [];
+  devCount = [];
+  custGroups = [];
+  custCount = [];
+  
+	//working standard deviation code - compacted
+	for (var i = 0; i < arrSize; i++) {
+		dataSum = dataSum + dataSet[i];
+	}
+	avg = dataSum / arrSize;
+	for (var i = 0; i < arrSize; i++) {
+		sumSqr = sumSqr + (dataSet[i] - avg) * (dataSet[i] - avg);
+	}
+	stdDev = sqrt(sumSqr / arrSize);
 
+	median();
+	minimumVal = dataSet[0];
+	maximumVal = dataSet[arrSize - 1];
 
+	//finds relative frequency for each unique number in data set
+	var temp = freq();
+	arrNums = temp[0];
+	arrFreqs = temp[1];
+	uniqueNums = arrNums.length;
+	relFreqs = [];
+	for (var i = 0; i < arrNums.length; i++) {
+		relFreqs[i] = arrFreqs[i] / arrSize;
+	}
+	maxNum = arrNums[arrNums.length - 1];
 
+	//for std dev horizontal
+	dataRange = dataSet[arrSize - 1] - dataSet[0];
+	for (var i = 0; i < dataRange / stdDev; i++) {
+		devGroups[i] = round(roundFactor * (stdDev * i + stdDev + minimumVal)) / roundFactor;
+		devCount[i] = 0;
+	}
 
+	for (var i = 0; i < arrSize; i++) {
+		for (var j = 0; j < devGroups.length; j++) {
+			if (
+				dataSet[i] < devGroups[j] &&
+				dataSet[i] >= devGroups[j] - round(roundFactor * stdDev) / roundFactor
+			) {
+				devCount[j] += 1;
+			}
+		}
+	}
 
+	//for custom horizontal
+	for (var i = 0; i < dataRange / customHistoWidth; i++) {
+		custGroups[i] =
+			round(
+				roundFactor * (customHistoWidth * i + customHistoWidth + minimumVal)
+			) / roundFactor; //round ( roundFactor * (stdDev * i + stdDev + minimumVal))/roundFactor;
+		custCount[i] = 0;
+	}
 
-
-
-
-
-
-
-
-
-
+	for (var i = 0; i < arrSize; i++) {
+		for (var j = 0; j < custGroups.length; j++) {
+			if (
+				dataSet[i] < custGroups[j] &&
+				dataSet[i] >=
+					custGroups[j] - round(roundFactor * customHistoWidth) / roundFactor
+			) {
+				custCount[j] += 1;
+			}
+		}
+	}
+}
 
